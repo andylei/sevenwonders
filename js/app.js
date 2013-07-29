95581b08b164b99e9e5de24f5ae980832a8b6cf1
Andy Lei <andylei@gmail.com>
2013-07-28 22:46:16 -0400

    styling

--- js/app.js
+++ js/app.js
@@ -9,10 +9,6 @@ Seven.ScoresRoute = Ember.Route.extend({
     controller.set('persons', [
       Seven.PersonScore.create(),
       Seven.PersonScore.create(),
-      Seven.PersonScore.create(),
-      Seven.PersonScore.create(),
-      Seven.PersonScore.create(),
-      Seven.PersonScore.create(),
       Seven.PersonScore.create()
     ]);
   }
@@ -30,10 +26,6 @@ Seven.ScoresController = Ember.Controller.extend({
 PERSON_SCORE_PARTS = ['war', 'money', 'wonders', 'civics', 'commerce', 'guilds', 'science'];
 Seven.PersonScore = Ember.Controller.extend({
   name: "",
-  nameCap: function() {
-    return this.get('name').toUpperCase();
-  }.property('name'),
-  parts: PERSON_SCORE_PARTS,
   total: function() {
     var s = 0;
     for (var i = 0; i < PERSON_SCORE_PARTS.length; i++) {
